32edaee9b054b5572a7d47d14f675f28a7c8c3f8
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-27 11:45:15 -0400

    solution

---
+++ js/trip.js
@@ -0,0 +1,9 @@
+var stay = prompt("Where would you like to stay?");
+
+var eat = prompt("What kind of food would you like to eat?");
+
+var time = prompt("How long will you be staying in nyc?");
+
+var visit = prompt("what sites would you like to see?");
+
+alert("You will be in NYC for " + time + "days. During that time you will stay at " + stay + " and eat " + eat + " and see " + visit + " ENJOY!");
