d39c0f36e2b29423238b37669936c69d9877e752
nadjathan <nadjanaomi@mac.com>
2016-04-02 17:47:44 +0000

    Done.

---
+++ js/trip.js
@@ -0,0 +1,6 @@
+var hotel = prompt("Where would you like to stay?");
+var sites = prompt("What sites do you want to visit? Seperate your answers by a comma.");
+var food = prompt("What food do you want to eat? Seperate your answers by a comma.");
+var nights = prompt("How many nights do you want to stay?");
+
+alert("They are staying at" + hotel +  ". They want to see " + sites + ". They want to eat" + food + ".They are staying for " +nights + ".");
